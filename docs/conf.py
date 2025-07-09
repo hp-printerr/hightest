24f8a948d60a14e5a8bc5b0283bd512c0a129ea5
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-07-09 19:41:07 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -11,5 +11,5 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 html_sidebars = {'**': []}
-html_title = "How to Activate Mytv on Your Device using amazon.com/mytv"
+
 
