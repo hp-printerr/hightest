8f5e48533792e63b9eaeee8f982f9b5c1a5ff83f
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-07-09 16:24:18 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,15 @@
+project = 'MyTV Registration Guide'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_css_files = [
+    'custom.css',
+]
 
