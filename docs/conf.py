37319747a011dff8cfc02de47d9c9c6e2b1b8dd8
hp-printerr <106080415+hp-printerr@users.noreply.github.com>
2025-07-10 19:52:09 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,17 +7,12 @@ extensions = []
 
 exclude_patterns = []
 
-
+html_theme = 'alabaster'
 html_static_path = ['_static']
 html_css_files = ['custom.css']
 html_sidebars = {'**': []}
 html_title = ""
 html_js_files = ['hide-version.js']
 templates_path = ['_templates']
-import sphinx_rtd_theme
-
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_js_files = ['remove-flyout.js']
 
 
